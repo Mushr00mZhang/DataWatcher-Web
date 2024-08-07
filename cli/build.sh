b1519cf38db86447eb40f081dca5c7ed49d520fc
比香菇傻一点 <392388987@qq.com>
2024-08-07 15:05:42 +0800

    添加docker相关文件

---
+++ cli/build.sh
@@ -0,0 +1,2 @@
+docker build . -t interfacewatcher-web
+docker save -o interfacewatcher_web.tar interfacewatcher-web
---
+++ cli/publish.sh
@@ -0,0 +1,3 @@
+docker load <interfacewatcher_web.tar
+docker compose down
+docker compose up -d --remove-orphans
